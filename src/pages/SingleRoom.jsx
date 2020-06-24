fce15fb7a799ec86b788fa4e5bec40ba90c9e872
MarcusDLG <delag010@gmail.com>
2020-06-24 13:47:28 -0400

    added styled-components, styled hero per room

--- src/pages/SingleRoom.jsx
+++ src/pages/SingleRoom.jsx
@@ -4,6 +4,7 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import { RoomContext } from "../Context";
+import StyledHero from "../components/StyledHero";
 
 class SingleRoom extends Component {
   constructor(props) {
@@ -41,13 +42,13 @@ class SingleRoom extends Component {
     } = room;
     console.log(room);
     return (
-      <Hero hero="roomsHero">
+      <StyledHero img={images[0]}>
         <Banner title={`${name} room`}>
           <Link to="/rooms" className="btn-primary">
-            Return Home
+            Back to Rooms!
           </Link>
         </Banner>
-      </Hero>
+      </StyledHero>
     );
   }
 }
