85436838dde92ddc73a94fa70d247e2b9f2a22a7
MarcusDLG <delag010@gmail.com>
2020-06-29 15:13:46 -0400

    Room filtering complete

--- src/Context.js
+++ src/Context.js
@@ -53,7 +53,7 @@ class RoomProvider extends Component {
   };
   handleChange = (event) => {
     const target = event.target;
-    const value = event.type === "checkbox" ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = event.target.name;
     this.setState(
       {
@@ -79,12 +79,26 @@ class RoomProvider extends Component {
     //transform value
     capacity = parseInt(capacity);
     // filter by type
-    if (type !== "All") {
+    if (type !== "all") {
       tempRooms = tempRooms.filter((room) => room.type === type);
     }
     // filter by capacity
     if (capacity !== 1) {
       tempRooms = tempRooms.filter((room) => room.capacity >= capacity);
+    }
+    // filter by price
+    tempRooms = tempRooms.filter((room) => room.price <= price);
+    //filter by size
+    tempRooms = tempRooms.filter(
+      (room) => room.size >= minSize && room.size <= maxSize
+    );
+    //filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter((room) => room.breakfast === true);
+    }
+    //filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter((room) => room.pets === true);
     }
 
     this.setState({
--- src/components/RoomsFilter.jsx
+++ src/components/RoomsFilter.jsx
@@ -23,7 +23,7 @@ const RoomsFilter = ({ rooms }) => {
   //get unique types
   let types = getUnique(rooms, "type");
   //add all
-  types = ["All", ...types];
+  types = ["all", ...types];
   // map to jsx
   types = types.map((item, index) => {
     return (
@@ -70,7 +70,59 @@ const RoomsFilter = ({ rooms }) => {
             {people}
           </select>
         </div>
-        {/* end select type */}
+        <div className="form-group">
+          <label htmlFor="price">room price ${price}</label>
+          <input
+            type="range"
+            name="price"
+            min={minPrice}
+            max={maxPrice}
+            id="price"
+            value={price}
+            onChange={handleChange}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="price">room size </label>
+          <div className="size-inputs">
+            <input
+              type="number"
+              name="minSize"
+              value={minSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+            <input
+              type="number"
+              name="maxSize"
+              value={maxSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="breakfast"
+              id="breakfast"
+              checked={breakfast}
+              onChange={handleChange}
+            />
+            <label htmlFor="breakfast">breakfast</label>
+          </div>
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="pets"
+              checked={pets}
+              onChange={handleChange}
+            />
+            <label htmlFor="breakfast">pets</label>
+          </div>
+        </div>
       </form>
     </section>
   );
